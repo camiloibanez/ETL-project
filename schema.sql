cd92c73a6ea85ef2306e82bc3b1efabe06a7e984
camiloibanez <camiloibanez8@gmail.com>
2020-05-28 20:01:05 -0500

    fmld tables

--- schema.sql
+++ schema.sql
@@ -6,6 +6,14 @@ DROP TABLE tmdb_production_countries;
 DROP TABLE tmdb_spoken_languages;
 DROP TABLE tmdb_movies;
 
+DROP TABLE FMLD_cast;
+DROP TABLE FMLD_crew;
+DROP TABLE FMLD_genre;
+DROP TABLE FMLD_production_companies;
+DROP TABLE FMLD_production_countries;
+DROP TABLE FMLD_spoken_languages;
+DROP TABLE FMLD_movies;
+
 
 CREATE TABLE tmdb_movies (
 	id DECIMAL PRIMARY KEY,
@@ -60,4 +68,59 @@ CREATE TABLE tmdb_spoken_languages (
 	id DECIMAL,
 	spoken_languages VARCHAR(100),
 	FOREIGN KEY (id) REFERENCES tmdb_movies(id)
+);
+
+CREATE TABLE FMLD_movies (
+	id DECIMAL PRIMARY KEY,
+	imdb_id VARCHAR(100),
+	tmdbId INT,
+	title VARCHAR(500),
+	release_date DATE,
+	revenue DECIMAL,
+	budget DECIMAL,
+	runtime DECIMAL,
+	original_language VARCHAR(100),
+	vote_average DECIMAL,
+	vote_count DECIMAL,
+	homepage VARCHAR(1000),
+	tagline VARCHAR(1000)
+);
+
+CREATE TABLE FMLD_cast (
+	movie_id DECIMAL,
+	character varchar,
+	cast_name varchar,
+	FOREIGN KEY (movie_id) REFERENCES FMLD_movies(id)
+);
+
+CREATE TABLE FMLD_crew (
+	id DECIMAL,
+	department VARCHAR(100),
+	job VARCHAR(100),
+	crew_name VARCHAR(100),
+	FOREIGN KEY (id) REFERENCES FMLD_movies(id)
+);
+
+CREATE TABLE FMLD_genre (
+	id DECIMAL,
+	genre VARCHAR(100),
+	FOREIGN KEY (id) REFERENCES FMLD_movies(id)
+);
+
+CREATE TABLE FMLD_production_companies (
+	id DECIMAL,
+	production_company VARCHAR(100),
+	FOREIGN KEY (id) REFERENCES FMLD_movies(id)
+);
+
+CREATE TABLE FMLD_production_countries (
+	id DECIMAL,
+	country VARCHAR(200),
+	FOREIGN KEY (id) REFERENCES FMLD_movies(id)
+);
+
+CREATE TABLE FMLD_spoken_languages (
+	id DECIMAL,
+	spoken_languages VARCHAR(100),
+	FOREIGN KEY (id) REFERENCES FMLD_movies(id)
 );
